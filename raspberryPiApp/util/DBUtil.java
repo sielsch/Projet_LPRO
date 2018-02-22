3ae42394587476d7abcf52dda25e519b4d3b9b54
Enzo-M <enzomena@gmail.com>
2018-02-22 21:31:54 +0100

    [add] dbExecuteQueryRasp and enregistrement des passages

--- raspberryPiApp/util/DBUtil.java
+++ raspberryPiApp/util/DBUtil.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
 import com.mysql.jdbc.Driver;
+import com.sun.rowset.CachedRowSetImpl;
 
 /**
  * Created by ONUR BASKIRT on 22.02.2016.
  */
 public class DBUtil {
+
     //Declare JDBC Driver
     private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
- 
+
     //Connection
     private static Connection conn = null;
- 
+
     //Connection String
     //String connStr = "jdbc:oracle:thin:Username/Password@IP:Port/SID";
     //Username=HR, Password=HR, IP=localhost, IP=1521, SID=xe
-   // private static final String connStr = "jdbc:oracle:thin:HR/HR@localhost:1521/xe";
- 
- 
+    // private static final String connStr = "jdbc:oracle:thin:HR/HR@localhost:1521/xe";
     //Connect to DB
     public static void dbConnect() throws SQLException, ClassNotFoundException {
         //Setting Oracle JDBC Driver
@@ -31,36 +31,36 @@ public class DBUtil {
             e.printStackTrace();
             throw e;
         }
- 
+
         System.out.println("Mysql JDBC Driver Registered!");
- 
+
         //Establish the Oracle Connection using Connection String
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root", "root");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
         } catch (SQLException e) {
             System.out.println("Connection Failed! Check output console" + e);
             e.printStackTrace();
             throw e;
         }
     }
- 
+
     //Close Connection
     public static void dbDisconnect() throws SQLException {
         try {
             if (conn != null && !conn.isClosed()) {
                 conn.close();
             }
-        } catch (Exception e){
-           throw e;
+        } catch (Exception e) {
+            throw e;
         }
     }
- 
+    
     //DB Execute Query Operation
-    public static List<String> dbExecuteQuery(String queryStmt) throws SQLException, ClassNotFoundException {
+    public static ResultSet dbExecuteQuery(String queryStmt) throws SQLException, ClassNotFoundException {
         //Declare statement, resultSet and CachedResultSet as null
         Statement stmt = null;
-        ResultSet rs = null;
-        List<String> Values = null;
+        ResultSet resultSet = null;
+        CachedRowSetImpl crs = null;
         try {
             //Connect to DB (Establish Oracle Connection)
             dbConnect();
@@ -70,18 +70,63 @@ public class DBUtil {
             stmt = conn.createStatement();
  
             //Execute select (query) operation
-            rs = stmt.executeQuery(queryStmt);
-	    ResultSetMetaData rsmd = rs.getMetaData();
-	    int columnsNumber = rsmd.getColumnCount();  
-	    System.out.println(columnsNumber+" columns");
-	    Values = new ArrayList<>();
-            while (rs.next()) {
-                for(int i=1;i<=columnsNumber;i++){
-                                 Values.add(rs.getString(i));
-                        }
-                
-                }
+            resultSet = stmt.executeQuery(queryStmt);
  
+            //CachedRowSet Implementation
+            //In order to prevent "java.sql.SQLRecoverableException: Closed Connection: next" error
+            //We are using CachedRowSet
+            crs = new CachedRowSetImpl();
+            crs.populate(resultSet);
+        } catch (SQLException e) {
+            System.out.println("Problem occurred at executeQuery operation : " + e);
+            throw e;
+        } finally {
+            if (resultSet != null) {
+                //Close resultSet
+                resultSet.close();
+            }
+            if (stmt != null) {
+                //Close Statement
+                stmt.close();
+            }
+            //Close connection
+            dbDisconnect();
+        }
+        //Return CachedRowSet
+        return crs;
+    }
+ 
+
+    //DB Execute Query Operation but returning a List because CachedRowSetImpl bug in RasperryPi
+    public static List<String[]> dbExecuteQueryRasp(String queryStmt) throws SQLException, ClassNotFoundException {
+        //Declare statement, resultSet and CachedResultSet as null
+        Statement stmt = null;
+        ResultSet rs = null;
+        List<String[]> Values = null;
+        String[] StringArray = null;
+        try {
+            //Connect to DB (Establish Oracle Connection)
+            dbConnect();
+            System.out.println("Select statement: " + queryStmt + "\n");
+
+            //Create statement
+            stmt = conn.createStatement();
+
+            //Execute select (query) operation
+            rs = stmt.executeQuery(queryStmt);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            System.out.println(columnsNumber + " columns");
+            Values = new ArrayList<>();
+            while (rs.next()) {
+                StringArray = new String[columnsNumber];
+                for (int i = 0; i < columnsNumber; i++) {
+                    StringArray[i]=rs.getString(i+1);
+                }
+                Values.add(StringArray);
+
+            }
+
         } catch (SQLException e) {
             System.out.println("Problem occurred at executeQuery operation : " + e);
             throw e;
@@ -100,7 +145,7 @@ public class DBUtil {
         //Return CachedRowSet
         return Values;
     }
- 
+
     //DB Execute Update (For Update/Insert/Delete) Operation
     public static void dbExecuteUpdate(String sqlStmt) throws SQLException, ClassNotFoundException {
         //Declare statement as null
@@ -124,4 +169,11 @@ public class DBUtil {
             dbDisconnect();
         }
     }
+    
+    public static void insertHistoriqueNow(String numBadge, int numZone) throws ClassNotFoundException, SQLException{
+		
+		String insert ="insert into DateAccesZone values ("+numBadge+", "+numZone+", CURRENT_TIMESTAMP);";
+		DBUtil.dbExecuteUpdate(insert);
+		
+	}
 }
